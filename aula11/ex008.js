8ecadbb0a3b93bfea0bd3a3707af05e606a4eb36
JamilyGSS <salesjamily0@gmail.com>
2025-04-04 16:06:41 -0300

    if e else

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.loc('O console funcionou corretamente!')
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel} km por hora`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Diriga sempre usando o cinto de segurança.')
